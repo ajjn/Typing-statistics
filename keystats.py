903427587a1d06d06e5f189fb3ce681cf239bfb0
Antti Nykänen <antti.nykanen@qvantel.com>
2019-11-15 11:28:32 +0200

    PEP8 and fix plotting for Mac

--- keystats.py
+++ keystats.py
@@ -1,87 +1,76 @@
-import tkinter as tk
-import time
+import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
+import tkinter as tk
+import time
+
+matplotlib.use('TkAgg')
+
 
 class KeyStats(tk.Tk):
     def __init__(self):
+        def next_row():
+            if not hasattr(self, 'row'):
+                self.row = 0
+            else:
+                self.row += 1
+            return self.row
+
         super().__init__()
         self.title("Key stats")
-        row = 0
-        tk.Label(self,
-                 text="""Click button to start and Enter to stop 
-                 recording""").grid(row=row,
-                                    columnspan=2,
-                                    padx=10,
-                                    pady=10)
-        self.status_text = tk.Label(self,
-                                    foreground="red",
-                                    text="")
-        row += 1
-        self.status_text.grid(row=row,
-                             columnspan=2,
-                             padx=10,
-                             pady=10)
-
-        self.stats_text = tk.Label(self,
-                              text="")
-        row += 1
-        self.stats_text.grid(row=row,
-                             columnspan=2,
-                             padx=10,
-                             pady=10)
-
-        row += 1
-        tk.Button(self,
-                  text="Start recording",
-                  command=self.toggle_status).grid(row=row,
-                                                   column=0,
-                                                   padx=10,
-                                                   pady=10)
-        tk.Button(self,
-                  text="Start simulation",
-                  command=self.simulate).grid(row=row,
-                                              column=1,
-                                              padx=10,
-                                              pady=10)
-
+        tk.Label(self, text="Click button to start and Enter to stop recording").grid(
+            row=next_row(),
+            columnspan=2,
+            padx=10,
+            pady=10
+        )
+        self.status_text = tk.Label(self, foreground="red", text="")
+        self.status_text.grid(row=next_row(), columnspan=2, padx=10, pady=10)
+        self.stats_text = tk.Label(self, text="")
+        self.stats_text.grid(row=next_row(), columnspan=2, padx=10, pady=10)
+        tk.Button(self, text="Start recording", command=self.toggle_status).grid(
+            row=next_row(),
+            column=0,
+            padx=10,
+            pady=10
+        )
+        tk.Button(self, text="Start simulation", command=self.simulate).grid(
+            row=self.row,
+            column=1,
+            padx=10,
+            pady=10
+        )
         self.textbox = tk.Text(self)
-        row += 1
-        self.textbox.grid(row=row,
-                          columnspan=2,
-                          padx=10,
-                          pady=10)
-        
-        
+        self.textbox.grid(row=next_row(), columnspan=2, padx=10, pady=10)
+
         self.bind("<Key>", self.key)
         self.times = []
         self.chars = []
-        self.status = False
+        self.recording_status = False
         self.after(500, self.update_status)
 
     def update_status(self):
-        if self.status:
-            if self.status_text['text'] == 'Recording':
-                self.status_text['text'] =''
-            else: self.status_text['text'] = 'Recording'
+        if self.recording_status:
+            self.status_text['text'] = 'Recording'
         else:
-            self.status_text['text'] =''
-        
+            self.status_text['text'] = ''
+
         self.update()
         self.after(500, self.update_status)
-        
+
     def initialize(self):
         self.textbox.delete(1.0, tk.END)
         self.chars = []
         self.times = []
 
-        
     def toggle_status(self):
-        self.status = not self.status
-        if not self.status:
+        self.recording_status = not self.recording_status
+        if not self.recording_status:
             self.times.append(time.time())
             self.plot_histogram()
             self.plot_char_stats()
+            plt.show()
+
         else:
             print("Started recording")
             self.initialize()
@@ -104,24 +93,19 @@ class KeyStats(tk.Tk):
         means = []
         stds = []
         labels = []
-        for k,v in char_dict.items():
+        for k, v in char_dict.items():
             labels.append(v.get_symbol())
             means.append(v.get_mean())
             stds.append(v.get_std())
 
-        
         ind = np.arange(len(labels))  # the x locations for the bars
         width = 0.7
         fig, ax = plt.subplots()
-        rects = ax.bar(ind, means, width, color='r', yerr=stds)
+        ax.bar(ind, means, width, color='r', yerr=stds)
 
         ax.set_ylabel('Mean interval [ms]')
         ax.set_title('Key specific interval lengths')
         plt.xticks(ind + 0.5*width, labels, rotation=60)
-        
-        #ax.set_xticklabels(labels)
-        plt.show()
-        
 
     def plot_histogram(self):
         if len(self.chars) < 2:
@@ -140,38 +124,36 @@ class KeyStats(tk.Tk):
         print("You typed {} characters.".format(len(intervals)+1))
         print("Mean of log of key press interval: {}".format(mu))
         print("Standard deviation of log of key press interval: {}".format(sigma))
-        bins = int(len(intervals)/10)
-        n, bins, patches = plt.hist(np.exp(intervals), normed=1,
-                                    facecolor='green', alpha=0.75)
-        l = plt.plot(bins, self.lognorm_pdf(bins, mu, sigma), 'r--', linewidth=1)
+        n, bins, patches = plt.hist(
+            np.exp(intervals),
+            facecolor='green',
+            alpha=0.75
+        )
+        plt.plot(bins, self.lognorm_pdf(bins, mu, sigma), 'r--', linewidth=1)
         plt.xlabel('Key press interval [s]')
-
-
         plt.ylabel('Normalized frequency')
-        plt.title("""Key press interval stats: 
-        mean = {:.2f} s, std = {:.2f} s""".format(orig_mu,
-                                                  orig_sigma))
-        #plt.axis([40, 160, 0, 0.03])
+        plt.title("Key press interval stats: mean = {:.2f} s, std = {:.2f} s".format(
+            orig_mu,
+            orig_sigma
+        ))
         plt.grid(True)
 
-        # Calculate char-wise stats
-        
-
     def lognorm_pdf(self, x, mu, sigma):
-        y = 1/(x*sigma*np.sqrt(2*np.pi))*np.exp(-(np.log(x)-mu)**2/(2*sigma**2))
+        y = 1 / (x * sigma * np.sqrt(2 * np.pi)) * np.exp(-(np.log(x) - mu) ** 2 / (2 * sigma ** 2))
         return y
-    
+
     def update_stats_text(self):
         if len(self.times) < 2:
             return
         times = np.array(self.times)
-        intervals = times[1:] - times[:-1] 
+        intervals = times[1:] - times[:-1]
         orig_mu = np.mean(intervals)
         orig_sigma = np.std(intervals)
-        self.stats_text['text'] = """Chars: {:d}, 
-        Mean: {:d} ms, Std: {:d} ms""".format(len(self.times),
-                                              int(1000 * orig_mu),
-                                              int(1000 * orig_sigma))
+        self.stats_text['text'] = "Chars: {:d}, Mean: {:d} ms, Std: {:d} ms".format(
+            len(self.times),
+            int(1000 * orig_mu),
+            int(1000 * orig_sigma)
+        )
 
     def simulate(self):
         times = np.array(self.times)
@@ -179,7 +161,7 @@ class KeyStats(tk.Tk):
         intervals = np.log(intervals)
         mu = np.mean(intervals)
         sigma = np.std(intervals)
-        intervals = np.exp(sigma * np.random.randn( len(self.chars)-1) + mu)
+        intervals = np.exp(sigma * np.random.randn(len(self.chars) - 1) + mu)
         self.textbox.delete(1.0, tk.END)
         self.textbox.insert(tk.END, self.chars[0])
         self.update()
@@ -195,13 +177,14 @@ class KeyStats(tk.Tk):
     def key(self, event):
         if event.char == '\r':
             self.toggle_status()
-        elif self.status:
+        elif self.recording_status:
             self.record(event.char)
-            
+
     def record(self, key):
         self.times.append(time.time())
         self.chars.append(key)
         self.update_stats_text()
+
 
 class Char:
     def __init__(self, symbol, interval):
@@ -213,17 +196,18 @@ class Char:
 
     def get_intervals(self):
         return self.intervals
-    
+
     def add_interval(self, interval):
         self.intervals.append(interval)
 
     def get_mean(self):
         # Value in milliseconds
         return 1000 * np.mean(np.array(self.get_intervals()))
-        
+
     def get_std(self):
         # Value in milliseconds
         return 1000 * np.std(np.array(self.get_intervals()))
-        
+
+
 app = KeyStats()
 app.mainloop()
--- typing_simulator.py
+++ typing_simulator.py
@@ -1,10 +1,9 @@
-import tkinter as tk
-from numpy import sqrt
-from numpy import log as ln
-from numpy import pi
-from numpy import exp
 import numpy as np
+from numpy import sqrt, exp
+from numpy import log as ln
 import time
+import tkinter as tk
+
 
 class TypingSimulator(tk.Tk):
     def __init__(self):
@@ -12,29 +11,38 @@ class TypingSimulator(tk.Tk):
 
         # Build interface
         row = 0
-        tk.Button(self,
-                  text="Start simulation",
-                  command=self.simulate).grid(row=row,
-                                              padx=10,
-                                              pady=10)
+        tk.Button(self, text="Start simulation", command=self.simulate).grid(
+            row=row,
+            padx=10,
+            pady=10
+        )
 
         self.textbox = tk.Text(self)
         row += 1
-        self.textbox.grid(row=row,
-                          padx=10,
-                          pady=10)
+        self.textbox.grid(row=row, padx=10, pady=10)
 
         # Build the texts
         self.texts = []
-        self.texts.append(SimulationText("""Typing of text is individual. """,
-                                         0.277, 0.23))
-        self.texts.append(SimulationText("""Some people type faster, """,
-                                         0.06, 0.04))
-        self.texts.append(SimulationText("""whereas some quite sloowwly...""",
-                                         0.4, 0.3))
-        self.texts.append(SimulationText(""" The interval between ket\by presses by human being can be consideeed \b\b\b\bred as a random variable. The interval can be simulated as a variable, whose logarithm is normally distributed. However, I don't know whether this is really true... One should study it... or maybe someone knows it already? Anyway, if you want to find out your typing statistics, you can try this little app. Have fun!""",
-                                         0.15, 0.17))
-        
+        self.texts.append(
+            SimulationText("Typing of text is individual. ", 0.277, 0.23)
+        )
+        self.texts.append(
+            SimulationText("Some people type faster, ", 0.06, 0.04)
+        )
+        self.texts.append(
+            SimulationText("whereas some quite sloowwly...", 0.4, 0.3)
+        )
+        self.texts.append(
+            SimulationText(" The interval between ket\by presses by human being can be consideeed"
+                           " \b\b\b\bred as a random variable. The interval can be simulated as a "
+                           "variable, whose logarithm is normally distributed. However, I don't "
+                           "know whether this is really true... One should study it... or maybe "
+                           "someone knows it already? Anyway, if you want to find out your typing "
+                           "statistics, you can try this little app. Have fun!",
+                           0.15,
+                           0.17)
+        )
+
     def simulate(self):
         # Clear the text box
         self.textbox.delete(1.0, tk.END)
@@ -44,10 +52,15 @@ class TypingSimulator(tk.Tk):
         intervals = np.array([])
         for text in self.texts:
             whole += text.get_text()
-            intervals = np.concatenate((intervals, self.random_lognormal_delay(text.get_mu(),
-                                                                               text.get_sigma(),
-                                                                               len(text.get_text()))))
-
+            intervals = np.concatenate(
+                (
+                    intervals,
+                    self.random_lognormal_delay(
+                        text.get_mu(),
+                        text.get_sigma(),
+                        len(text.get_text()))
+                )
+            )
 
         # Run the simulation
         for i in range(len(whole)):
@@ -58,15 +71,14 @@ class TypingSimulator(tk.Tk):
                 self.textbox.insert(tk.END, whole[i])
             self.update()
             time.sleep(intervals[i])
-            
-                
 
     def random_lognormal_delay(self, m, sd, n):
         # calculate mu and sigma
-        mu = ln(m/sqrt(1+sd**2/m**2))
-        sigma = sqrt(ln(1+sd**2/m**2))
+        mu = ln(m / sqrt(1 + sd ** 2 / m ** 2))
+        sigma = sqrt(ln(1 + sd ** 2 / m ** 2))
         return exp(sigma * np.random.randn(n) + mu)
-        
+
+
 class SimulationText:
     def __init__(self, text, mu, sigma):
         self.text = text
@@ -81,6 +93,7 @@ class SimulationText:
 
     def get_sigma(self):
         return self.sigma
+
 
 app = TypingSimulator()
 app.mainloop()
